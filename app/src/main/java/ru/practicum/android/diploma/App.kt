4fc7f2bb2053c118f221c7636db2a3646304ec7e
Stuysha <styusha.kinski@yandex.ru>
2024-03-29 17:24:24 +0300

    fix detekt

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -3,4 +3,9 @@ package ru.practicum.android.diploma
 import android.app.Application
 
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // заготовка для настройки библиотек
+    }
 }
