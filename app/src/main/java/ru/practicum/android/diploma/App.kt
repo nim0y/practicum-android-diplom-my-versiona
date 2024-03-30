3922005461cb238367cb27ca831a858fa571bdad
alex1261bat <alex126.1bat@gmail.com>
2024-03-30 16:48:33 +0300

    Добавлен Koin и модули

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -1,11 +1,31 @@
 package ru.practicum.android.diploma
 
 import android.app.Application
+import org.koin.core.context.startKoin
+import org.koin.android.ext.koin.androidContext
+import ru.practicum.android.diploma.di.dataBaseModule
+import ru.practicum.android.diploma.di.interactorModule
+import ru.practicum.android.diploma.di.navigationModule
+import ru.practicum.android.diploma.di.networkModule
+import ru.practicum.android.diploma.di.repositoryModule
+import ru.practicum.android.diploma.di.sharedPreferencesModule
+import ru.practicum.android.diploma.di.viewModelModule
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        // заготовка для настройки библиотек
+        startKoin {
+            androidContext(this@App)
+            modules(
+                dataBaseModule,
+                interactorModule,
+                navigationModule,
+                networkModule,
+                repositoryModule,
+                sharedPreferencesModule,
+                viewModelModule
+            )
+        }
     }
 }
