076e6a50867bcc09d03a9edb0a808cae792739c9
nim0y <130760051+nim0y@users.noreply.github.com>
2024-03-31 00:59:17 +0300

    Detekt fix 3

--- app/src/main/java/ru/practicum/android/diploma/data/network/client/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/client/RetrofitNetworkClient.kt
@@ -7,7 +7,6 @@ import ru.practicum.android.diploma.data.network.api.Request
 import ru.practicum.android.diploma.data.network.api.Request.MainSearchRequest
 import ru.practicum.android.diploma.data.network.api.Response
 import ru.practicum.android.diploma.util.Constants.CODE_SUCCESS
-import ru.practicum.android.diploma.util.isConnected
 
 class RetrofitNetworkClient(
     private val context: Context,
@@ -15,9 +14,7 @@ class RetrofitNetworkClient(
 ) : NetworkClient {
 
     override suspend fun doRequest(request: Request): Response {
-        if (!isConnected(context)) {
-            return Response().apply { resultCode = -1 }
-        }
+       
         var response = Response()
         return try {
             response = when (request) {
