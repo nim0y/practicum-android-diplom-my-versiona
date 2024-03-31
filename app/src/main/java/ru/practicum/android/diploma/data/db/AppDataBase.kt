64e4081eb9216af7257eebfe33785de6707a9595
alex1261bat <alex126.1bat@gmail.com>
2024-03-31 13:47:56 +0300

    fix: исправил detekt

--- app/src/main/java/ru/practicum/android/diploma/data/db/AppDataBase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AppDataBase.kt
@@ -7,9 +7,9 @@ import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyEntity
 
 @Database(
     version = 1,
-    entities = [FavoriteVacancyEntity::class])
+    entities = [FavoriteVacancyEntity::class]
+)
 abstract class AppDataBase : RoomDatabase() {
-
     companion object {
         const val DB_NAME = "appDataBase.db"
     }
