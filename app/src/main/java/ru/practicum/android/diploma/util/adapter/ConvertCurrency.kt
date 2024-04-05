8e3ea025dc9f2e1ebf2b0e129f0a8531f384e893
Stuysha <styusha.kinski@yandex.ru>
2024-04-05 19:33:13 +0300

    Реализован конвектор валют

---
+++ app/src/main/java/ru/practicum/android/diploma/util/adapter/ConvertCurrency.kt
@@ -0,0 +1,58 @@
+package ru.practicum.android.diploma.util.adapter
+
+object ConvertCurrency {
+
+    fun formatValueSalary(salaryFrom: Int?, salaryTo: Int?): String {
+        return when {
+            (salaryFrom != null && (salaryTo == null || salaryTo == 0)) -> "от ${toSalaryString(salaryFrom)}"
+            ((salaryFrom == 0 || salaryFrom == null) && (salaryTo != null && salaryTo != 0)) -> "до ${
+                toSalaryString(
+                    salaryTo
+                )
+            }"
+
+            ((salaryFrom != null && salaryFrom != 0) && (salaryTo != null && salaryTo != 0)) -> "от ${
+                toSalaryString(
+                    salaryFrom
+                )
+            } до ${toSalaryString(salaryTo)}"
+
+            else -> {
+                "Зарплата не указана"
+            }
+        }
+    }
+
+    fun toSalaryString(salary: Int): String {
+        val salarySize = salary.toString().count() - 1
+        var result = ""
+        var cycle = 0
+        for (i in salarySize downTo 0) {
+            cycle = cycle + 1
+            if (cycle == 3) {
+                cycle = 0
+                result = " " + salary.toString()[i] + result
+            } else {
+                result = salary.toString()[i] + result
+            }
+        }
+        return result.trim()
+    }
+
+    fun converterSalaryToString(salaryFrom: Int?, salaryTo: Int?, currency: String?): String {
+        val formattedSalary = formatValueSalary(salaryFrom, salaryTo)
+        return when (currency) {
+            "USD" -> "$formattedSalary €"
+            "EUR" -> "$formattedSalary $"
+            "RUR" -> "$formattedSalary ₽"
+            "AZN" -> "$formattedSalary ₼"
+            "BYR" -> "$formattedSalary Br"
+            "GEL" -> "$formattedSalary ₾"
+            "KGS" -> "$formattedSalary с"
+            "KZT" -> "$formattedSalary ₸"
+            "UAH" -> "$formattedSalary ₴"
+            "UZS" -> "$formattedSalary Soʻm"
+            else -> formattedSalary
+        }
+    }
+}
