0d696ff91c4cb439d57d0757f7bcfe675866d577
nim0y <nim0y33@gmail.com>
2024-04-05 20:07:21 +0300

    Detekt round 4
    Первичная настройка поиска и отображения результатов.
    По UI есть 2 бага пока не знаю как решать.
    1 Отображение валют
    2 При повторной фокусировке на searchQuery navigation bar
    находится наверху клавиатуры. Смещяя все, думаю про Scrollview
     но с пагинацией не знаю как будет работать

--- app/src/main/java/ru/practicum/android/diploma/data/network/client/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/client/RetrofitNetworkClient.kt
@@ -1,11 +1,14 @@
 package ru.practicum.android.diploma.data.network.client
 
 import android.content.Context
+import retrofit2.HttpException
 import ru.practicum.android.diploma.data.network.api.HeadHuntersApi
 import ru.practicum.android.diploma.data.network.api.Request
 import ru.practicum.android.diploma.data.network.api.Request.MainSearchRequest
 import ru.practicum.android.diploma.data.network.api.Response
 import ru.practicum.android.diploma.util.Constants.CODE_SUCCESS
+import ru.practicum.android.diploma.util.Constants.NO_CONNECTION_ERROR
+import ru.practicum.android.diploma.util.isConnected
 
 class RetrofitNetworkClient(
     private val context: Context,
@@ -13,6 +16,9 @@ class RetrofitNetworkClient(
 ) : NetworkClient {
 
     override suspend fun doRequest(request: Request): Response {
+        if (!isConnected(context)) {
+            return Response().apply { resultCode = NO_CONNECTION_ERROR }
+        }
         var response = Response()
         return try {
             response = when (request) {
@@ -33,8 +39,8 @@ class RetrofitNetworkClient(
                 }
             }
             response.apply { resultCode = CODE_SUCCESS }
-        } catch (exception: java.net.UnknownHostException) {
-            return Response().apply { resultCode = -1 }
+        } catch (exception: HttpException) {
+            response.apply { resultCode = exception.code() }
         }
     }
 }
