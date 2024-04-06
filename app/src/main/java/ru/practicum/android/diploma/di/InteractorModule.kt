7bc49c05a339b810042126fa8d683e9a18d9e82e
STRATEG <lacredin@gmail.com>
2024-04-06 16:04:34 +0300

    fix merge

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.api.SearchInteractor
+import ru.practicum.android.diploma.domain.impl.SearchInteractorImpl
 import ru.practicum.android.diploma.domain.sharing.SharingInteractor
 import ru.practicum.android.diploma.domain.sharing.impl.SharingInteractorImpl
 
@@ -8,5 +10,8 @@ val interactorModule = module {
 
     single<SharingInteractor> {
         SharingInteractorImpl(externalNavigator = get())
+    }
+    single<SearchInteractor> {
+        SearchInteractorImpl(get())
     }
 }
